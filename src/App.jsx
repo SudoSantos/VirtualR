b4d15facdfbe40bd3496e8c5623acc0ff4a07309
kanu Chukwuemeka <mekuseo@gmail.com>
2024-10-06 12:43:02 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// import React from 'react'
+
+import FeatureSection from "./components/FeatureSection";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Workflow from "./components/Workflow";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection/> 
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+      </div>
+     
+    </>
+  );
+};
+
+export default App;
